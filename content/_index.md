cd67b2c4d50c4d8756fa37de3c5ccbdea33f3e21
Tfferdous <ferdous.themefisher@gmail.com>
2022-12-19 17:00:55 +0600

    banner section added

--- content/_index.md
+++ content/_index.md
@@ -1,4 +1,11 @@
 ---
 banner:
-  title: "Next Tailwind Boilerplate"
+  title: Welcome <strong>!</strong> </br> <span>to Jhon Bravo Blog</span>
+  content: Are Developer and recently started your own business Already made website to ensure presence wants to develop.
+  image: /images/banner.png
+  button:
+    label: Know About Me
+    link: /about
+    rel: ""
+  
 ---
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import config from "@config/config.json";
 import Base from "@layouts/Baseof";
+import ImageFallback from "@layouts/components/ImageFallback";
 import { getListPage, getSinglePage } from "@lib/contentParser";
 import { sortByDate } from "@lib/utils/sortFunctions";
 import { markdownify } from "@lib/utils/textConverter";
 import Post from "@partials/Post";
+import Link from "next/link";
 const { blog_folder } = config.settings;
 
 const Home = ({ banner, posts, authors }) => {
@@ -12,9 +14,38 @@ const Home = ({ banner, posts, authors }) => {
 
   return (
     <Base>
-      <div className="section">
+      {/* Banner */}
+      <section className="section">
         <div className="container">
-          {markdownify(banner.title, "h1", "h2 mb-8 text-center")}
+          <div className="row flex-wrap-reverse items-center justify-center lg:flex-row">
+            <div className="mt-12 text-center lg:mt-0 lg:text-left lg:col-6">
+              {markdownify(banner.title, "h1", "h2 banner-title")}
+              {markdownify(banner.content, "p", "mt-4")}
+              <Link
+                className="btn btn-primary mt-6"
+                href={banner.button.link}
+                rel={banner.button.rel}
+              >
+                {banner.button.label}
+              </Link>
+            </div>
+            <div className="col-9 lg:col-6">
+              <ImageFallback
+                className="mx-auto"
+                src={banner.image}
+                width={548}
+                height={443}
+                priority={true}
+                alt="Banner Image"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Posts */}
+      <section>
+        <div className="container">
           <div className="row">
             {sortPostByDate.slice(0, showPost).map((post, i) => (
               <div key={`key-${i}`} className="col-12 mb-8 sm:col-6">
@@ -23,7 +54,7 @@ const Home = ({ banner, posts, authors }) => {
             ))}
           </div>
         </div>
-      </div>
+      </section>
     </Base>
   );
 };
